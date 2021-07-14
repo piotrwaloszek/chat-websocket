e7b09b02a1e19ceb587f8ddd299dec1154aa9eab
Piotr Wałoszek <piotrek.waloszek@gmail.com>
2021-07-14 11:07:42 +0200

    Add information about log and log out

--- client/app.js
+++ client/app.js
@@ -1,29 +1,40 @@
+const socket = io();
+
+socket.on('message', ({ author, content }) => addMessage(author, content));
+socket.on('join', (event) => addMessage('ChatBot', `${event.user} has joined the conversation!`));
+socket.on('removeUser', (event) => addMessage('ChatBot', `${event.user} has left the conversation... :(`));
+
 const loginForm = document.querySelector('#welcome-form');
 const messagesSection =document.querySelector('#messages-section');
 const messagesList = messagesSection.querySelector('#messages-list');
 const addMessageForm = messagesSection.querySelector('#add-messages-form');
 const userNameInput = loginForm.querySelector('#username');
 const messageContentInput = addMessageForm.querySelector('#message-content');
-
 let userName = '';
-
 const login = (e) => {
     e.preventDefault();
     if(userNameInput.value === userName){
         alert('Please enter Your login');
     } else {
         userName = userNameInput.value;
+        let id = socket.id;
         loginForm.classList.remove('show');
         messagesSection.classList.add('show');
+        socket.emit('join', { user: userName, id: id });
     }
-     
+
 }
 loginForm.addEventListener('submit', login);
 
 function addMessage(author, content) {
     const message = document.createElement('li');
-    message.classList.add('message');
-    author === userName ? message.classList.add('message--self') : message.classList.add('message--recieved'); 
+    if(author === 'ChatBot'){
+        message.setAttribute('class', 'message message--chatBot')
+    } else if (author === userName){
+        message.setAttribute('class', 'message message--self')
+    } else {
+        message.setAttribute('class', 'message message--recieved')
+    };
     message.innerHTML = 
         `<h3 class="message__author">${userName === author ? 'You' : author }</h3>
         <div class="message__content">
@@ -31,13 +42,13 @@ function addMessage(author, content) {
         </div>`;
     messagesList.appendChild(message);
   }
-
 const sendMessage = (e) => {
     e.preventDefault();
     if(messageContentInput.value === ''){
         alert('Please type Your message');
     } else{
         addMessage(userName, messageContentInput.value);
+        socket.emit('message', { author: userName, content: messageContentInput.value })
         messageContentInput.value = '';
     } 
 }
--- server.js
+++ server.js
@@ -1,22 +1,55 @@
+const { createSocket } = require('dgram');
 const express = require('express');
 const path = require('path');
+const socket = require('socket.io');
 
 const app = express();
 
 app.use(express.urlencoded({extended: false}));
-app.use(express.json());
+//app.use(express.json());
 app.use(express.static(path.join(__dirname, './client')));
 
 const messages = [];
+const users = [];
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, './client/index.html'));
   });
-
 app.use((req, res) => {
     res.status(404).send('404 not found...')
 });
 
-app.listen(8000, () => {
-    console.log('Server is running on port 8000')
+const server = app.listen(8000, () => {
+    console.log('Server is running on Port:', 8000)
+  });
+  const io = socket(server);
+
+  io.on('connection', (socket) => {
+    console.log('New client! Its id – ' + socket.id);
+    socket.on('message', (message) => {
+        console.log('Oh, I\'ve got something from ' + socket.id);
+        messages.push(message);
+        socket.broadcast.emit('message', message);
+    });
+
+    socket.on('join', (user) => {
+        users.push(user);
+        console.log(user.user);
+        socket.broadcast.emit('join', user);
+    })
+
+    socket.on('disconnect', () => {
+        // socket.broadcast.emit('removeUser', userToRemove);
+        // let logout = users.indexOf(users.find(item => item.id === socket.id));
+        // users.splice(logout, 1);
+        // console.log('Oh, socket ' + socket.id + ' has left') 
+        users.forEach(user => {
+            if(user.id == socket.id){
+                const index = users.indexOf(user);
+                socket.broadcast.emit('removeUser', user);
+                users.splice(index, 1);
+            }
+        }); 
+    });
+    console.log('I\'ve added a listener on message and disconnect events \n');
 });
